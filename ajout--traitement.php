c995f4c4790afd85e9ee6f3032c20763f2a2e15e
Pradeilles Chloé <74190557+Cpradlls@users.noreply.github.com>
2020-11-26 15:33:59 +0100

    Update ajout--traitement.php

--- ajout--traitement.php
+++ ajout--traitement.php
@@ -7,18 +7,20 @@
 
   // affiche un message si un champ n'est pas rempli
   if ($prixLitre == false) exit("merci de renseigner le prix au litre, payé à la pompe svp");
+  if ($volumePlein == false) exit("merci de renseigner le volume total du plein (en litres), mis à la pompe svp");
+  if ($kmParcourus == false) exit("merci de renseigner les kilomètres parcourus, depuis le dernier plein effectué svp");
 
   // calcul à effectuer
-  $moyenneLitresAuCent = "TODO";
+  $moyenneLitresAuCent = $volumePlein*100/$kmParcourus;
 
   // date à cet instant T
   $dateObjet = new DateTime(null, new DateTimeZone("Europe/Paris"));
-  $datePlein = $dateObjet->format("Y-m-d H:i:s.u");
+  $datePlein = $dateObjet->format("Y-m-d");
 
   // dépose un nouveau cookie
   $cookieIndex = $dateObjet->format("YmdHisu");
   $cookieContent = "$datePlein|$prixLitre|$moyenneLitresAuCent";
-  setcookie("plein[$cookieIndex]", $cookieContent, strtotime( "+365 days" ));
+  setcookie("plein[$cookieIndex]", $cookieContent, strtotime( "+182 days" ));
 
   // redirection vers la page d'accueil
   header("Location: index.php");
