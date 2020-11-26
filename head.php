71df742889c2de837d2a7fdaa8aee216f96e28dc
Pradeilles Chloé <74190557+Cpradlls@users.noreply.github.com>
2020-11-26 15:33:23 +0100

    Update head.php

--- head.php
+++ head.php
@@ -5,4 +5,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>suivi-carbu — auto++</title>
   <meta name="description" content="Suivez facilement l'évolution de la consommation de votre véhicule, un plein à la fois !">
+  <link rel="stylesheet" href="style.css">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Yanone+Kaffeesatz:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
 </head>
